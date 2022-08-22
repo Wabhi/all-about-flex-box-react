05e9f90bdb35afebffe9a78cf408bc695e145288
ABHISHEK TIWARI <tiwariabhishek27611@gmail.com>
2022-08-22 12:10:04 +0000

    Update FlexBox-One.css and FlexBox-One.js

--- src/FlexBox-One.js
+++ src/FlexBox-One.js
@@ -4,6 +4,7 @@ const FlexBoxOne = ()=>{
     return (
         <>
         <h1>Flex Box with Nav</h1>
+        <hr className="horizantl-line"/>
         <nav>
             <ul className="nav-list">
                 <li><a href="#">Home</a></li>
@@ -16,6 +17,7 @@ const FlexBoxOne = ()=>{
             </ul>
         </nav>
         <h1>Flex Box with Card</h1>
+        <hr className="horizantl-line"/>
         <div className="columns">
             <article className='card'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -28,6 +30,22 @@ const FlexBoxOne = ()=>{
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </article>
         </div>
+       <h1>Flex Box with Box</h1>
+       <hr className="horizantl-line"/>
+        <div className="boxes">
+            <div className="box"></div>
+            <div className="box"></div>
+            {/* <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div> */}
+        </div>
+        <h1>Flex Box with Box, center a box within box</h1>
+        <hr className="horizantl-line"/>
+        <div className='outter-div'>
+            <div className="inner-div"></div>
+        </div>
+
         </>
     )
 }
